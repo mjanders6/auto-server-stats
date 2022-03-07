36f2c1212d8b0c70d517b4136989faaa1cf154ed
mjanders6 <mjanders6@gmail.com>
2022-03-06 19:15:39 -0800

    shell commands

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ require('dotenv').config()
 
 const { prompt } = require('inquirer')
 const Table = require('cli-table');
+const { exec } = require('child_process');
 
 // instantiate
 var table = new Table({
     head: ['TH 1 label', 'TH 2 label']
-  , colWidths: [100, 200]
+    , colWidths: [25, 50]
 });
+
+
 
 // table is an Array, so you can `push`, `unshift`, `splice` and friends
 table.push(
-    ['First value', 'Second value']
-  , ['First value', 'Second value']
+    ['CPU', 'Second value']
+    , ['Hard Drive', 'Second value']
 );
 
-console.log(table.toString());
+//console.log(table.toString());
+
+
+exec('ls | grep js', (err, stdout, stderr) => {
+    if (err) {
+        //some err occurred
+        console.error(err)
+    } else {
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+});
