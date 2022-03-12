599c6554619319f4eb65bf788849fe8c4bea1ec6
mjanders6 <mjanders6@gmail.com>
2022-03-11 23:39:22 -0800

    update to functions

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const si = require('systeminformation');
+const Table = require('cli-table');
+
+let cpuData = () => {
+
+    var table = new Table({
+        head: ['Function', 'Info']
+        , colWidths: [25, 50]
+    });
+
+    si.cpu()
+        .then(data => {
+            let brand = data.brand;
+            let manu = data.manufacturer;
+            let vendor = data.vendor;
+            let speed = data.speed;
+            let cores = data.cores;
+
+            return table.push(['CPU', [brand, manu]])
+        })
+        .catch(error => console.error(error));
+
+}
+
+
+
