021502c8c16d005bdd9b5492fa07709402fd9750
mjanders6 <mjanders6@gmail.com>
2022-03-11 21:24:55 -0800

    updated app.js

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-require('dotenv').config()
+#!/usr/bin/env node
 
-const { prompt } = require('inquirer')
-const Table = require('cli-table');
-const { exec } = require('child_process');
-
-// instantiate
-var table = new Table({
-    head: ['TH 1 label', 'TH 2 label']
-    , colWidths: [25, 50]
-});
-
-
-
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
-table.push(
-    ['CPU', 'Second value']
-    , ['Hard Drive', 'Second value']
-);
-
-console.log(table.toString());
-
-
-exec('sudo vgs', (err, stdout, stderr) => {
-    if (err) {
-        //some err occurred
-        console.error(err)
-    } else {
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    }
-});
+console.log('hello')
