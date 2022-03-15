4a9ec2744e7f503d93807ca0afcf23b6b67a28a5
mjanders6 <mjanders6@gmail.com>
2022-03-15 12:13:26 -0700

    using columnify instead of cli-tables becuase you can have a multi line cell

--- app.js
+++ app.js
@@ -2,33 +2,71 @@
 
 const si = require('systeminformation');
 const Table = require('cli-table');
+const columnify = require('columnify')
 
 var comp_Data = new Array()
 
-var table = new Table({
-    head: ['Function', 'Info']
-    , colWidths: [25, 150]
-});
+// var table = new Table({
+//     head: ['Function', 'Info']
+//     , colWidths: [25, 150]
+// });
 
 async function compData() {
     try {
         const cpuData = await si.cpu();
         const memData = await si.mem();
-        const system = await si.system();
+        // const system = await si.system();
         const bios = await si.bios();
         const motherboard = await si.baseboard();
-        const os = await si.osInfo();
+        // const os = await si.osInfo();
         const users = await si.users();
 
-        table.push(
-            { User: [`User: ${users[0].user}`]},
-            { CPU: [`Brand: ${cpuData.manufacturer} ${cpuData.brand} | Speed: ${cpuData.speedMax}`] },
-            { Memory: [`Free: ${memData.free/1000000000} | Used: ${memData.used/1000000000} | Total: ${memData.total/1000000000}`] },
-            { System: [`Manufacturer: ${system.manufacturer} | Model: ${system.model}`]},
-            { BIOS: [`Vendor: ${bios.vendor} | Version: ${bios.version} | Release Date: ${bios.releaseDate} | Revision: ${bios.revision}`]},
-            { Motherboard: [`Manufacturer: ${motherboard.manufacturer} | Model: ${motherboard.model} | Version: ${motherboard.version} | Memory Slots: ${motherboard.memSlots}`]},
-            { OS: [`Op. System: ${os.platform} | Distro: ${os.distro} | Release: ${os.release} | Host Name: ${os.hostname} | Kernal: ${os.kernel}`]}
-        )
+        var table = [{
+            name: `User`,
+            paths: [`${users[0].user}`]
+        }, {
+            name: 'CPU',
+            paths: [
+                `Manufacturer: ${cpuData.manufacturer}`,
+                `Brand: ${cpuData.brand}`,
+                `Cores: ${cpuData.cores}`,
+                `Speed: ${cpuData.speed}`,
+                `Max Speed: ${cpuData.speedMax}`
+            ].join('\n')
+        }, {
+            name: `Memory`,
+            paths: [
+                `Free: ${memData.free / 1000000000}`,
+                `Used: ${memData.used / 1000000000}`,
+                `Total: ${memData.total / 1000000000}`
+            ].join('\n')
+        }, {
+            name: `BIOS`,
+            paths: [
+                `Vendor: ${bios.vendor}`,
+                `Version: ${bios.version}`,
+                `Release Date: ${bios.releaseDate}`,
+                `Revision: ${bios.revision}`
+            ].join('\n')
+        }, {
+            name: `Motherboard`,
+            paths: [
+                `Manufacturer: ${motherboard.manufacturer}`,
+                `Model: ${motherboard.model}`,
+                `Version: ${motherboard.version}`,
+                `Memory Slots: ${motherboard.memSlots}`
+            ].join('\n')
+        }]
+
+        // table.push(
+        //     { User: [`User: ${users[0].user}`]},
+        //     { CPU: [`Brand: ${cpuData.manufacturer} ${cpuData.brand} | Speed: ${cpuData.speedMax}`] },
+        //     { Memory: [`Free: ${memData.free/1000000000} | Used: ${memData.used/1000000000} | Total: ${memData.total/1000000000}`] },
+        //     { System: [`Manufacturer: ${system.manufacturer} | Model: ${system.model}`]},
+        //     { BIOS: [`Vendor: ${bios.vendor} | Version: ${bios.version} | Release Date: ${bios.releaseDate} | Revision: ${bios.revision}`]},
+        //     { Motherboard: [`Manufacturer: ${motherboard.manufacturer} | Model: ${motherboard.model} | Version: ${motherboard.version} | Memory Slots: ${motherboard.memSlots}`]},
+        //     { OS: [`Op. System: ${os.platform} | Distro: ${os.distro} | Release: ${os.release} | Host Name: ${os.hostname} | Kernal: ${os.kernel}`]}
+        // )
         return table
     } catch (e) {
         console.log(e)
@@ -38,6 +76,6 @@ async function compData() {
 
 compData()
     .then(data => {
-        console.log(data.toString())
+        console.log(columnify(data, { preserveNewLines: true }))
     })
     .catch(e => console.error(e))
